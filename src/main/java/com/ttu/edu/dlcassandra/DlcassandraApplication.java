6f7886e4e211bee589ee375c77056a7c67bb9c80
karthik9966 <navakarthikreddy@gmail.com>
2023-05-26 15:18:16 -0500

    Initializing Repository with basic files

---
+++ src/main/java/com/ttu/edu/dlcassandra/DlcassandraApplication.java
@@ -0,0 +1,13 @@
+package com.ttu.edu.dlcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlcassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlcassandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttu/edu/dlcassandra/DlcassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttu.edu.dlcassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlcassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
