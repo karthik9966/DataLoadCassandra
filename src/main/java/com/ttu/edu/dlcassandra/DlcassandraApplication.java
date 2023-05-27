d12ddc8a78f99110315f08334723ca08b377dc57
karthik9966 <navakarthikreddy@gmail.com>
2023-05-27 16:52:57 -0500

    Adding application.yml file to connect to cassandra cluster

--- src/main/java/com/ttu/edu/dlcassandra/DlcassandraApplication.java
+++ src/main/java/com/ttu/edu/dlcassandra/DlcassandraApplication.java
@@ -1,13 +1,26 @@
 package com.ttu.edu.dlcassandra;
 
+import com.ttu.edu.dlcassandra.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class DlcassandraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DlcassandraApplication.class, args);
 	}
 
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties dataStaxAstraProperties)
+	{
+		Path bundle = dataStaxAstraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
 }
